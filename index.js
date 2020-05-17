b740ffa6f073762bd21a3c766256f24f9ce3b801
Leonard Esere <leonardesere@gmail.com>
2020-05-16 20:55:27 -0500

    decleared the class for theis code and also added the content for  the time elements

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ $('.date').text(`${output}`)
 // 
 // 
 
+$(".planner").click(function() {
 $('.moal-container').toggle();
 
 // Displaying the modal container for our page. 
@@ -28,6 +29,8 @@ $('.modal-container--close').click(function() {
 
     $('.modal-container').css('display', 'none');
 }) 
+
+
 
 
 // opt[value='$@5 AM']{$@5 AM}*7;
@@ -42,7 +45,50 @@ $('.modal-container--close').click(function() {
 
 //THEN I am presented with timeblocks for standard business hours
 //WHEN I view the timeblocks for that day
+let row = 0;
+let colume = 0;
+let lenght = 0;
 
+
+///////////////// Regex Expressions //////////////////////////////
+//// Drawing Idea from https://www.rexegg.com/regex-quickstart.html/////
+
+colume = e.target.planes.match(/\d+/) [0];
+
+
+///////////////// Regex Expressions Ends /////////////////////////
+
+////////// SETTING THE TIMEOBJ ////////////// 
+
+let timeObj = {
+
+"5 AM " : 1,
+"6 AM " : 2,
+"7 AM " : 3,
+"8 AM " : 4,
+"9 AM " : 5,
+"10 AM " : 6,
+"11 AM " : 7,
+"12 NOON " : 8,
+"1 PM " : 9,
+"2 PM " : 10,
+"3 PM " : 11,
+"4 PM " : 12,
+"5 PM " : 13,
+"6 PM " : 14,
+"7 PM " : 15,
+"8 PM " : 16,
+"9 PM " : 17,
+
+
+
+
+
+
+
+
+
+}
 //THEN each timeblock is color coded to indicate whether it is in the past,       present, or future
 //WHEN I click into a timeblock
 
@@ -54,6 +100,6 @@ $('.modal-container--close').click(function() {
 
 //THEN the saved events persist
 
-
+});
 
 })
