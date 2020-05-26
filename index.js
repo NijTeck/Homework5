475e3f316f7c59cbf30d7d25015efe7e69d9dba0
Regeditted <36388273+NijTeck@users.noreply.github.com>
2020-05-25 23:07:01 -0500

    Made some update
    to the index.html and index to js file.. did some css styling also. 
    I have made some major changes to the file.

--- index.js
+++ index.js
@@ -1,110 +1,94 @@
-$(function() {
+        // create timeBox element (contains time)
+        const $timeBoxSpn = $('<span>');
+        // can use this to get value
+        $timeBoxSpn.attr('class', 'timeBox');
+
+        // format hours for display
+        let displayHour = 0;
+        let ampm = "";
+        if (hour > 12) {
+            displayHour = hour - 12;
+            ampm = "pm";
+        } else {
+            displayHour = hour;
+            ampm = "am";
+        }
+
+        $(document).ready(function() {
+
+                    // test flag
+                    const test = false;
+
+                    // get times from moment
+                    const now = moment().format('MMMM Do YYYY');
+
+                    // commented out for test in non-standard hours
+                    let nowHour24 = moment().format('H');
+                    let nowHour12 = moment().format('h');
+
+                    // set times for tesitng after hours
+                    if (test) {
+                        nowHour24 = 13;
+                        nowHour12 = 1;
+                    }
+
+                    let $dateHeading = $('#navbar-subtitle');
+                    $dateHeading.text(now);
 
 
-    // Showing the date of today to the  user
+                    $('.date').text(`${output}`)
 
-    let d = new Date();
+                    $(".planNow").click(function(e) {
 
-    // SETTING THE YEAR, MONTH AND DATE  TO  SHOW UP ON THE TOP OF THE PAGE
+                        let row = 0,
+                            length = 1,
+                            column = 0
 
-    let toYear = d.getFullYear();
-    let toMonth = d.getMonth()+1;
-    let toDay = d.getDate();
+                        let timeObj = {
+                            "5 AM": 1,
+                            "6 AM": 2,
+                            "7 AM": 3,
+                            "8 AM": 4,
+                            "9 AM": 5,
+                            "10 AM": 6,
+                            "11 AM": 7,
+                            "12 PM": 8,
+                            "1 PM": 9,
+                            "2 PM": 10,
+                            "3 PM": 11,
+                            "4 PM": 12,
+                            "5 PM": 13,
+                            "6 PM": 14,
+                            "7 PM": 15,
+                            "8 PM": 16,
+                            "9 PM": 17,
+                            "10 PM": 18,
+                            "11 PM": 19,
+                            "12 AM": 20,
+                            "1 AM": 21,
+                            "2 AM": 22,
+                            "3 AM": 23,
+                            "4 AM": 24
+                        };
 
-    let totalDays = new Date(toYear, toMonth, 0).getDate();
+                        column = e.target.className.match(/\d+/)[0];
 
-    let output =  (toMonth<10 ? '0' : ' ') + toMonth + '/' + (toDay <10 ? '0' : ' ') + toDay + '/' + toYear
+                        $('.modal-container').toggle();
 
-$('.date').text(`${output}`)
+                        $('.modal-container--close').click(function() {
+                            $('.modal-container').css('display', 'none')
+                        });
 
-// 
-// 
+                        $('.schedule-container--form').submit(function(e) {
+                            e.preventDefault();
 
-$(".planner").click(function() {
-$('.moal-container').toggle();
-
-// Displaying the modal container for our page. 
-
-$('.modal-container--close').click(function() {
-
-    $('.modal-container').css('display', 'none');
-    let row = 0, length = 1, column = 0
-}) 
-
-
-
-
-// opt[value='$@5 AM']{$@5 AM}*7;
-
-
-
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner
-
-//THEN the current day is displayed at the top of the calendar
-//WHEN I scroll down
-
-//THEN I am presented with timeblocks for standard business hours
-//WHEN I view the timeblocks for that day
-let row = 0;
-let colume = 0;
-let lenght = 0;
-
-
-///////////////// Regex Expressions //////////////////////////////
-//// Drawing Idea from https://www.rexegg.com/regex-quickstart.html/////
-
-colume = e.target.planes.match(/\d+/) [0]; // Pulled info also from  https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-ii-7af7d239a55d
+                            row = timeObj[$("#start-start").val()] // calling the  id="modal-start" to select the from time
 
 
-///////////////// Regex Expressions Ends /////////////////////////
-
-////////// SETTING THE TIMEOBJ ////////////// 
-
-let timeObj = {
-
-"5 AM " : 1,
-"6 AM " : 2,
-"7 AM " : 3,
-"8 AM " : 4,
-"9 AM " : 5,
-"10 AM " : 6,
-"11 AM " : 7,
-"12 NOON " : 8,
-"1 PM " : 9,
-"2 PM " : 10,
-"3 PM " : 11,
-"4 PM " : 12,
-"5 PM " : 13,
-"6 PM " : 14,
-"7 PM " : 15,
-"8 PM " : 16,
-"9 PM " : 17,
-"10 PM" : 18,
-"11 PM" : 19,
-"12 AM" : 20,
-"1 AM" : 21,
-"2 AM" : 22,
-"3 AM" : 23,
-"4 AM" : 24
-}
-
-
-// PUll some code snip from   https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-ii-7af7d239a55d
-// Because the INFO directly relates to what I was trying to achieve. 
-
-column = e.target.className.match(/\d+/)[0]
-
-        $('.modal-container').toggle()
-
-        $('.modal-container--close').click(function(){
-            $('.modal-container').css('display', 'none')
-        })
-        
-        $('.modal-container--form').submit(function(e){
-            e.preventDefault()
-
-length = (timeObj[$( "#end-select" ).val()]) - (timeObj[$( "#start-select" ).val()])
-
-
-})
+                            length = (timeObj[$("#end-end").val()]) - (timeObj[$("#start-start").val()]);
+                            $('.content').append(`<div class='event' style='grid-row:${row}/span ${length};grid-column:${column};background-color: yellow;'>Reservation</div>`);
+                            $('.modal-container--form')[0].reset();
+                            $('.modal-container--form').off();
+                            $('.modal-container').toggle();
+                        });
+                    });
